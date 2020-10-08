6b90c503815b3184dcc6f261af5fc9b46e3d730f
Oliver Steiner <oliver.steiner@htl-wels.at>
2020-10-08 11:21:57 +0200

    Finish commit

--- src/at/sto/Main.java
+++ src/at/sto/Main.java
@@ -3,25 +3,24 @@ package at.sto;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Geben sie 2 Zahlen ein, die Sie addieren wollen: \n");
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        double num1, num2;
-        try {
-            System.out.printf("Zahl 1: %n");
-            num1 = scanner.nextDouble();
+        int num1, num2;
+        for (int i = 0; i < 1000; i++) {
+            try {
+                System.out.println("Geben sie 2 Zahlen ein, die Sie addieren wollen: \n");
+                System.out.printf("Zahl 1: %n");
+                num1 = scanner.nextInt();
 
-            System.out.printf("Zahl 2: %n");
-            num2 = scanner.nextDouble();
+                System.out.printf("Zahl 2: %n");
+                num2 = scanner.nextInt();
 
-            double result = num1 + num2;
-            System.out.printf("Ergebnis: %f,12", result);
-        }
-        catch (Exception e)
-        {
-            System.out.println("Die Eingabe ist Fehlerhaft");
+                int result = num1 + num2;
+                System.out.printf("Ergebnis: %d%n", result);
+            } catch (Exception e) {
+                System.out.println("Die Eingabe ist Fehlerhaft");
+            }
         }
     }
 }
