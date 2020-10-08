70a9ada26264af6d36aad8da3007a9a16adf490a
Oliver Steiner <oliver.steiner@htl-wels.at>
2020-10-08 10:57:55 +0200

    First commit

---
+++ src/at/sto/Main.java
@@ -0,0 +1,21 @@
+package at.sto;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Geben sie 2 Zahlen ein, die Sie addieren wollen: \n");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int num1, num2;
+
+        System.out.printf("Zahl 1: %n");
+        num1 = scanner.nextInt();
+
+        System.out.printf("Zahl 2: %n");
+        num2 = scanner.nextInt();
+    }
+}
+
+
